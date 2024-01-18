61f0bc7ff0a958a010386db8339a2241d166eb23
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2024-01-18 09:06:41 -0600

    modify static url

--- src/main/java/com/chris/microservices/userservice/api/APIClient.java
+++ src/main/java/com/chris/microservices/userservice/api/APIClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "department-service", url = "http://localhost:8000")
+@FeignClient(value = "department-service")
 public interface APIClient {
     @GetMapping(value = "/api/departments/{id}")
     DepartmentDto getDepartmentById(@PathVariable("id") Long departmentId);
